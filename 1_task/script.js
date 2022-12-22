cf5b4dced80a8771350c7c629e02066d0804735f
Anastasiya3895641 <anastasiya-shitenkova@mail.ru>
2022-12-23 00:34:54 +0300

    1 task is finish

--- 1_task/script.js
+++ 1_task/script.js
@@ -1,103 +1,33 @@
-function checkCatsForm () {
-    fetch('https://httpbin.org/post', {
+const url = 'https://httpbin.org/post';
+
+window.onerror = function(message, url, lineNumber) {
+  alert("Catch error in Global Internet!\n" +
+    "Massege: " + message + "\n(" + url + ":" + lineNumber + ")");
+};
+
+function checkCatsForm (){
+    fetch(url, {
         method: 'POST',
         body: new FormData(form),
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
-          },
     })
     .then((response) => {
-        if (response.ok) {
-          return response.json();
+      try{
+        if (!response.ok) {
+          throw new Error('Something went wrong, try again');
         }
-        throw new Error('Something went wrong');
-      })
-      //.then((responseJson) => {
-        // Do something with the response
-      //})
-      .catch((e) => {
-        alert( "JSON Error: " + e.message ); 
-      });
-
-};
-document.getElementById('button').addEventListener('click',checkCatsForm);
-
-/*async function getUsers(names) {
-    let jobs = [];
-  
-    for(let name of names) {
-      let job = fetch(`https://api.github.com/users/${name}`).then(
-        successResponse => {
-          if (successResponse.status != 200) {
-            return null;
-          } else {
-            return successResponse.json();
-          }
-        },
-        failResponse => {
+        
+      } catch(error) {
+        alert( "JSON Error: " + error.message); 
+      }
+      try{
+        if (response.status != 200) {
           return null;
         }
-      );
-      jobs.push(job);
+          throw new Error('Something went wrong, try again');
+      } catch(error) {
+        alert( "Status Error: " + error.message); 
+      }
+        });
     }
-  
-    let results = await Promise.all(jobs);
-  
-    return results;
-  }
-*/
+document.getElementById('button').addEventListener('click',checkCatsForm);
 
-
-
-
-
-  /*fetch('https://httpbin.org/post', {
-        method: 'POST',
-        body: new FormData(form),
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
-          },
-    })
-    .then(responce => responce.json())
-    .catch(err => console.error(err))
-    */
-
-
-   /* fetch('https://httpbin.org/post', {
-    method: 'POST',
-    body: new FormData(form),
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
-      },
-})
-.then((response) => {
-    if (response.ok) {
-      return response.json();
-    }
-    throw new Error('Something went wrong');
-  })
-  //.then((responseJson) => {
-    // Do something with the response
-  //})
-  .catch((e) => {
-    alert( "JSON Error: " + e.message ); 
-  }); */
-
-
-
-  /*const url = 'https://httpbin.org/post';
-const data = { body: 'example' };
-
-try {
-const response = await fetch(url, {
-method: 'POST', 
-body: JSON.stringify(data), // данные могут быть 'строкой' или {объектом}!
-headers: {
-'Content-Type': 'application/json'
-}
-});
-const json = await response.json();
-console.log('Успех:', JSON.stringify(json));
-} catch (error) {
-console.error('Ошибка:', error);
-} */
